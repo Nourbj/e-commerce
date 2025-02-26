29ff6de64835ea0c7416266f9e13de060b23bd05
Nour Ben Jannena <benjannena.n@decade.fr>
2025-02-26 18:03:01 +0100

    Page home: update logo

--- src/app/Components/Server/Header.jsx
+++ src/app/Components/Server/Header.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Search from './Search';
 import CartComponent from '../Client/CartComponent';
+import Link from 'next/link';
 
 
 const Header = () => {
@@ -10,17 +11,19 @@ const Header = () => {
         <div className="row align-items-center d-flex justify-content-between">
           <div className="col-sm-4 d-flex align-items-center">
             <div className="logo">
-              <h1>
-                <Image 
-                  src="/img/logo.png" 
-                  alt="Logo" 
-                  className="img-fluid" 
-                  width={150}  
-                  height={50}  
-                  priority 
-                  style={{ width: 'auto', height: 'auto' }}
-                />
-              </h1>
+            <Link href="/">
+                <h1>
+                  <Image 
+                    src="/img/logo.png" 
+                    alt="Logo" 
+                    className="img-fluid" 
+                    width={150}  
+                    height={50}  
+                    priority 
+                    style={{ width: 'auto', height: 'auto' }}
+                  />
+                </h1>
+              </Link>
             </div>
           </div>
           <div className="col-sm-0 d-flex justify-content-center">
